33eae08f1f201b477f8c5ef037a4b6b1753698c4
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-03-30 15:46:34 +0200

    activated routing service

--- frontend-app/src/app/app-routing.module.ts
+++ frontend-app/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// tasks 
+import { TaskCreateComponent } from './components/tasks/task-create/task-create.component';
+import { TaskListComponent } from './components/tasks/task-list/task-list.component';
+import { TaskEditComponent } from './components/tasks/task-edit/task-edit.component';
+// stories
+import { StoryCreateComponent } from './components/stories/story-create/story-create.component';
+import { StoryListComponent } from './components/stories/story-list/story-list.component';
+import { StoryEditComponent } from './components/stories/story-edit/story-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'stories-list'},
+  {path: 'create-story', component: StoryCreateComponent},
+  {path: 'edit-story/:id', component: StoryEditComponent},
+  {path: 'stories-list', component: StoryListComponent},
+  {path: 'create-task', component: TaskCreateComponent},
+  {path: 'edit-task/:id', component: TaskEditComponent},
+  {path: 'tasks-list', component: TaskListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend-app/src/app/app.module.ts
+++ frontend-app/src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoryCreateComponent } from './components/story-create/story-create.component';
-import { StoryEditComponent } from './components/story-edit/story-edit.component';
-import { StoryListComponent } from './components/story-list/story-list.component';
+import { StoryCreateComponent } from './components/stories/story-create/story-create.component';
+import { StoryEditComponent } from './components/stories/story-edit/story-edit.component';
+import { StoryListComponent } from './components/stories/story-list/story-list.component';
+import { TaskCreateComponent } from './components/tasks/task-create/task-create.component';
+import { TaskEditComponent } from './components/tasks/task-edit/task-edit.component';
+import { TaskListComponent } from './components/tasks/task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StoryCreateComponent,
     StoryEditComponent,
-    StoryListComponent
+    StoryListComponent,
+    TaskCreateComponent,
+    TaskEditComponent,
+    TaskListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
